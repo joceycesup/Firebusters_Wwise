56f8de521149f72828dba6479a9cd7b2469e4d0b
Thibault Baron <thibault.baron56@gmail.com>
2017-06-07 19:44:53 +0200

    id.h resolve

---
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID P1_ISLOUIS = 3856636393U;
+        static const AkUniqueID P1_ISMARIUS = 268287134U;
+        static const AkUniqueID PLAY_AXE_CHARIOT_HIT = 4273581253U;
+        static const AkUniqueID PLAY_AXE_MAILBOX_HIT = 3699785949U;
+        static const AkUniqueID PLAY_AXE_WOOD_HIT = 2979634928U;
+        static const AkUniqueID PLAY_FIRE = 3015324718U;
+        static const AkUniqueID PLAY_MARIONNETTE_AUTOHIT = 35712357U;
+        static const AkUniqueID PLAY_MARIONNETTE_HIT_BOIS = 2997904568U;
+        static const AkUniqueID PLAY_MARIONNETTE_HIT_CASSEROLE = 399661800U;
+        static const AkUniqueID PLAY_MARIONNETTE_HIT_CERAMIQUE = 838427783U;
+        static const AkUniqueID PLAY_MARIONNETTE_HIT_CHANDELIER = 3079512572U;
+        static const AkUniqueID PLAY_MARIONNETTE_HIT_FAUTEUIL = 1716381292U;
+        static const AkUniqueID PLAY_MARIONNETTE_HIT_METAL = 339153334U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_SQUEAK = 1357217968U;
+        static const AkUniqueID PLAY_VAISSELLE_BREAK = 3256339846U;
+        static const AkUniqueID START_EXTINCTEUR = 278157641U;
+        static const AkUniqueID STOP_EXTINCTEUR = 2840216739U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace AUTOHIT_VELOCITE
+        {
+            static const AkUniqueID GROUP = 1394615607U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FAIBLE = 1470352910U;
+                static const AkUniqueID FORT = 2947616894U;
+                static const AkUniqueID MOYEN = 487420215U;
+            } // namespace SWITCH
+        } // namespace AUTOHIT_VELOCITE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID FIRE_COUNT = 892026609U;
+        static const AkUniqueID LUSTRE_ANGLE = 337608746U;
+        static const AkUniqueID PLAYERSELECTIONPANNING = 1782474839U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID VELOCITE = 3519441204U;
+        static const AkUniqueID VELOCITE_PROPS = 3927383607U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
